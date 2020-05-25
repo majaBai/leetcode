e99b385ad1ffddc6c281201d1f0cf8fc770d2950
MajaBai <452712285@qq.com>
2020-05-25 20:44:12 +0800

    Container With Most Water

--- practice/src/MedianOfArrays.java
+++ practice/src/MedianOfArrays.java
@@ -47,6 +47,89 @@ public class MedianOfArrays {
         else return (double) total[(len - 1) / 2];
     }
 
+    public static double findMedianSortedArrays1(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m > n) { // to ensure m<=n
+            int[] temp = A;
+            A = B;
+            B = temp;
+            int tmp = m;
+            m = n;
+            n = tmp;
+        }
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && B[j - 1] > A[i]) {
+                iMin = i + 1; // i is too small
+            } else if (i > iMin && A[i - 1] > B[j]) {
+                iMax = i - 1; // i is too big
+            } else { // i is perfect
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                } else if (j == 0) {
+                    maxLeft = A[i - 1];
+                } else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = Math.min(B[j], A[i]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+
+    public static double findMedianSortedArrays3(int[] input1, int[] input2) {
+//        ensure input1.length smaller than input2
+        if(input1.length > input2.length){
+            return findMedianSortedArrays3(input2, input1);
+        }
+
+        int x = input1.length;
+        int y = input2.length;
+
+        int low = 0;
+        int high = x;
+        while(low<=high){
+            int partitionX = (low + high) /2;
+            int partitionY = (x+y+1)/2 - partitionX;
+
+            int maxLeftX = (partitionX == 0)?Integer.MIN_VALUE : input1[partitionX -1];
+            int minRightX = (partitionX == x)? Integer.MAX_VALUE : input1[partitionX];
+
+            int maxLeftY = (partitionY == 0)? Integer.MIN_VALUE : input2[partitionY -1];
+            int minRightY = (partitionY == y)? Integer.MAX_VALUE : input2[partitionY];
+
+            if(maxLeftX <= minRightY && maxLeftY <= minRightX){
+                if((x+y)%2 == 0){
+                    return ((double)Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY))/2;
+                } else {
+                    return (double)Math.max(maxLeftX, maxLeftY);
+                }
+            } else if(maxLeftX > minRightY){
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
     public static void main(String[] args) {
         {
             int[] nums1 = {1, 3};
