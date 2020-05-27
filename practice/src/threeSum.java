0dcc63b00a0f173aeb03f252cd566214b2776180
MajaBai <452712285@qq.com>
2020-05-27 17:16:46 +0800

    3 sum

--- practice/src/threeSum.java
+++ practice/src/threeSum.java
@@ -17,66 +17,28 @@ A solution set is:
 * */
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class threeSum {
 
-    public static List<List<Integer>> towSum(int[] nums, int target, HashMap<Integer, Integer> hm, int j) {
-        List<List<Integer>> result = new ArrayList();
-        for (int i = 0; i < nums.length; i++) {
-            if (i != j) {
-                int dif = target - nums[i];
-                int idx = hm.getOrDefault(dif, -1);
-                if (idx != -1 && idx != j && idx != i) {
-                    List<Integer> temp = new ArrayList();
-                    temp.add(nums[i]);
-                    temp.add(nums[idx]);
-                    temp.add(nums[j]);
-                    result.add(temp);
+    public static List<List<Integer>> threeSum(int[] num){
+        Arrays.sort(num);
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < num.length-2; i++) {
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                        while (lo < hi && num[lo] == num[lo+1]) lo++;
+                        while (lo < hi && num[hi] == num[hi-1]) hi--;
+                        lo++; hi--;
+                    } else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
                 }
             }
         }
-        return result;
-    }
-
-    public static boolean containsList(List<List<Integer>> a, ArrayList<Integer> b){
-        boolean bool = false;
-        for(List<Integer> arr : a){
-            if(arr.containsAll(b) && b.containsAll(arr)){
-                bool = true;
-                break;
-            }
-        }
-        return bool;
-    }
-
-    public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> r = new ArrayList();
-        var hm = new HashMap<Integer, Integer>(nums.length);
-        for (int i = 0; i < nums.length; i++) {
-            hm.put(nums[i], i);
-        }
-        for (int i = 0; i < nums.length; i++) {
-            int f = nums[i];
-            List t = towSum(nums, -f, hm, i);
-            if (!t.isEmpty()) {
-                if(t.size() == 1){
-                    if(r.isEmpty() || !containsList(r, (ArrayList<Integer>) t.get(0))){
-                        r.add((List<Integer>)t.get(0));
-                    }
-                } else {
-                    for(int k = 0; k < t.size(); k++){
-                        if(r.isEmpty() || !containsList(r, (ArrayList<Integer>)t.get(k))){
-                            r.add((ArrayList<Integer>)t.get(k));
-                        }
-                    }
-                }
-
-            }
-        }
-        return r;
+        return res;
     }
 
     public static void main(String[] args) {
