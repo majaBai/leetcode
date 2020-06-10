f67156630153927985b8f7b244d8bdfd09c5c41d
Maja <452712285@qq.com>
2020-06-10 21:17:54 +0800

    2nd method for search in rotated array

--- practice/src/SearchInRotatedArray.java
+++ practice/src/SearchInRotatedArray.java
@@ -16,27 +16,60 @@ Output: 4
 * */
 public class SearchInRotatedArray{
 
-    public static int search(int[] nums, int target){
-        if(nums.length == 0) return -1;
-        int lo = 0, hi = nums.length -1;
+//    public static int search(int[] nums, int target){
+//        if(nums.length == 0) return -1;
+//        int lo = 0, hi = nums.length -1;
+//        while(lo < hi){
+//            int mid = (lo + hi)/2;
+//            if(nums[mid] == target) return mid;
+//            if(nums[lo] <= nums[mid]){
+//                if(target < nums[mid] && target >= nums[lo]){
+//                    hi = mid - 1;
+//                } else {
+//                    lo = mid +1;
+//                }
+//            } else {
+//                if(target > nums[mid] && target <= nums[hi]){
+//                    lo = mid+1;
+//                } else {
+//                    hi = mid -1;
+//                }
+//            }
+//        }
+//        return nums[lo] == target? lo : -1;
+//    }
+
+    private static  int getMinIndex(int[] nums){
+        int lo = 0;
+        int hi = nums.length -1;
         while(lo < hi){
             int mid = (lo + hi)/2;
-            if(nums[mid] == target) return mid;
-            if(nums[lo] <= nums[mid]){
-                if(target < nums[mid] && target >= nums[lo]){
-                    hi = mid - 1;
-                } else {
-                    lo = mid +1;
-                }
+            if(nums[mid] < nums[hi]){
+                hi = mid;
             } else {
-                if(target > nums[mid] && target <= nums[hi]){
-                    lo = mid+1;
-                } else {
-                    hi = mid -1;
-                }
+                lo = mid +1;
             }
         }
-        return nums[lo] == target? lo : -1;
+        return hi;
+    }
+
+    public static int search (int[] nums, int target){
+        int n = nums.length;
+        if(n == 0) return -1;
+        int minIndex = getMinIndex(nums);
+        return target > nums[n - 1]
+                ? binarySearch(nums,target, 0, minIndex - 1)
+                : binarySearch(nums, target, minIndex, n-1);
+    }
+
+    private static int binarySearch(int[] nums, int target,int lo, int hi){
+        while(lo <= hi){
+            int mid = (lo + hi) / 2;
+            if(nums[mid] == target) return mid;
+            else if (nums[mid] > target) hi = mid -1;
+            else lo = mid + 1;
+        }
+        return -1;
     }
 
     public static void main(String[] args){
