18d36e38302ed1e3ed6c01f98bdf530b98cd8144
Maja <452712285@qq.com>
2020-06-21 12:00:42 +0800

    optimize: ensure time complexity lg(m+n)

--- practice/src/MedianOfArrays.java
+++ practice/src/MedianOfArrays.java
@@ -47,57 +47,11 @@ public class MedianOfArrays {
         else return (double) total[(len - 1) / 2];
     }
 
-    public static double findMedianSortedArrays1(int[] A, int[] B) {
-        int m = A.length;
-        int n = B.length;
-        if (m > n) { // to ensure m<=n
-            int[] temp = A;
-            A = B;
-            B = temp;
-            int tmp = m;
-            m = n;
-            n = tmp;
-        }
-        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
-        while (iMin <= iMax) {
-            int i = (iMin + iMax) / 2;
-            int j = halfLen - i;
-            if (i < iMax && B[j - 1] > A[i]) {
-                iMin = i + 1; // i is too small
-            } else if (i > iMin && A[i - 1] > B[j]) {
-                iMax = i - 1; // i is too big
-            } else { // i is perfect
-                int maxLeft = 0;
-                if (i == 0) {
-                    maxLeft = B[j - 1];
-                } else if (j == 0) {
-                    maxLeft = A[i - 1];
-                } else {
-                    maxLeft = Math.max(A[i - 1], B[j - 1]);
-                }
-                if ((m + n) % 2 == 1) {
-                    return maxLeft;
-                }
-
-                int minRight = 0;
-                if (i == m) {
-                    minRight = B[j];
-                } else if (j == n) {
-                    minRight = A[i];
-                } else {
-                    minRight = Math.min(B[j], A[i]);
-                }
-
-                return (maxLeft + minRight) / 2.0;
-            }
-        }
-        return 0.0;
-    }
-
-    public static double findMedianSortedArrays3(int[] input1, int[] input2) {
+    public static double findMedianSortedArrays2(int[] input1, int[] input2) {
+        if(input1.length == 0 || input2.length == 0) throw new IllegalArgumentException("wrong arguments: arrays can not be empty");
 //        ensure input1.length smaller than input2
         if(input1.length > input2.length){
-            return findMedianSortedArrays3(input2, input1);
+            return findMedianSortedArrays2(input2, input1);
         }
 
         int x = input1.length;
@@ -127,37 +81,50 @@ public class MedianOfArrays {
                 low = partitionX + 1;
             }
         }
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("wrong arguments: arrays can not be empty");
     }
 
     public static void main(String[] args) {
         {
             int[] nums1 = {1, 3};
             int[] nums2 = {2};
-            double median = findMedianSortedArrays(nums1, nums2);
-            assert median == 2.0;
+            double median = findMedianSortedArrays2(nums1, nums2);
+            System.out.println(median);
         }
+        System.out.println("-----");
         {
             int[] nums1 = {1, 2};
             int[] nums2 = {3, 4};
-            double median = findMedianSortedArrays(nums1, nums2);
-            assert median == 2.5;
+            double median = findMedianSortedArrays2(nums1, nums2);
+            System.out.println(median);
         }
+        System.out.println("-----");
         {
             int[] nums1 = {12};
             int[] nums2 = {12};
-            double median = findMedianSortedArrays(nums1, nums2);
-            assert median == 12.0;
+            double median = findMedianSortedArrays2(nums1, nums2);
+            System.out.println(median);
         }
+        System.out.println("-----");
+        {
+            int[] nums1 = {1, 3, 5, 7, 9};
+            int[] nums2 = {3, 4, 5, 6};
+            double median = findMedianSortedArrays2(nums1, nums2);
+            System.out.println(median);
+        }
+        System.out.println("-----");
         {
             int[] nums1 = {};
             int[] nums2 = {1};
-            double median = findMedianSortedArrays(nums1, nums2);
+            double median = findMedianSortedArrays2(nums1, nums2);
+            System.out.println(median);
         }
+        System.out.println("-----");
         {
             int[] nums1 = {1};
             int[] nums2 = {};
-            double median = findMedianSortedArrays(nums1, nums2);
+            double median = findMedianSortedArrays2(nums1, nums2);
+            System.out.println(median);
         }
     }
 }
