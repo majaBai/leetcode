4da8fff910c069872e863d6881d43dc82f2e841d
Maja <452712285@qq.com>
2020-06-23 16:23:53 +0800

    optimize valid anagram

--- practice/src/ValidAnagram.java
+++ practice/src/ValidAnagram.java
@@ -3,13 +3,26 @@ import java.util.Arrays;
 
 public class ValidAnagram{
 
-    public static boolean isAnagram(String s, String t){
+    public static boolean isAnagram_1(String s, String t){
        if(s.length() != t.length()) return false;
        char[] a = s.toCharArray();
        char[] b = t.toCharArray();
        Arrays.sort(a);
        Arrays.sort(b);
        return Arrays.equals(a, b);
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        int[] table = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+            table[t.charAt(i) - 'a']--;
+        }
+        for(int count : table){
+            if(count != 0) return false;
+        }
+        return true;
     }
 
     public static void main(String[] args){
