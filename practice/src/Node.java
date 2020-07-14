4d566e85cf14de8e1d3fe9781bbac650cf74893a
Maja <452712285@qq.com>
2020-07-15 05:05:47 +0800

    class node with random

---
+++ practice/src/Node.java
@@ -0,0 +1,19 @@
+public class Node{
+
+    int val;
+    Node next;
+//    random pointer which could point to any node in the list or null.
+    Node random;
+
+
+    public Node(int val){
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+
+    public static void main(String[] args){
+        
+
+    }
+}
