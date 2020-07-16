e8319200efbe2999d2eba7f8371cc1e824203953
Maja <452712285@qq.com>
2020-07-16 16:35:18 +0800

    add description

--- practice/src/IntersectionOfTwoList.java
+++ practice/src/IntersectionOfTwoList.java
@@ -1,5 +1,25 @@
+/*160. Intersection of Two Linked Lists
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+For example, the following two linked lists:
+  Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+  Output: Reference of the node with value = 8
+  Input Explanation:
+* The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+* From the head of A, it reads as [4,1,8,4,5].
+* From the head of B, it reads as [5,6,1,8,4,5].
+* There are 2 nodes before the intersected node in A;
+* There are 3 nodes before the intersected node in B.
+
+* Notes:
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Each value on each linked list is in the range [1, 10^9].
+Your code should preferably run in O(n) time and use only O(1) memory.
+* */
+
 import java.util.HashSet;
-import java.util.List;
 
 public class IntersectionOfTwoList{
 
