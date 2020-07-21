9564b14b0fbaefef402c47c872f9d791990a7a64
Maja <452712285@qq.com>
2020-07-21 14:27:53 +0800

    maxSubarray: solution2

--- practice/src/MaxSubarray.java
+++ practice/src/MaxSubarray.java
@@ -7,6 +7,7 @@ Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 
+follow up:
 * If you have figured out the O(n) solution,
 * try coding another solution using the divide and conquer approach, which is more subtle.
 * */
@@ -14,13 +15,35 @@ Explanation: [4,-1,2,1] has the largest sum = 6.
 public class MaxSubarray{
 
     public static int solution(int[] nums){
-        int maxsofar = Integer.MIN_VALUE, leftPositive = 0;
-        for(int i = 0; i < nums.length; i++){
-            maxsofar = Math.max(maxsofar, leftPositive + nums[i]);
-            leftPositive = Math.max(0, leftPositive + nums[i]);
-        }
-        return maxsofar;
+       int maxSofar = Integer.MIN_VALUE, sumLeft = 0;
+       for(int i = 0; i < nums.length; i++){
+           maxSofar = Math.max(maxSofar, sumLeft+nums[i]);
+           sumLeft = Math.max(0, sumLeft+nums[i]);
+       }
+       return maxSofar;
     }
+
+    // divide and conquer
+    public static int solution2(int[] nums){
+        return maxSubArray(nums, 0, nums.length-1);
+    }
+
+    public static int maxSubArray(int[] nums, int lo, int hi){
+        if(lo > hi) return Integer.MIN_VALUE;
+        int mid = lo+(hi-lo)/2,ml = 0, mr = 0;
+        int lmax = maxSubArray(nums, lo, mid-1);
+        int rmax = maxSubArray(nums, mid+1, hi);
+        for(int i = mid-1, sum =0; i >= lo; i--){
+            sum += nums[i];
+             ml = Math.max(sum, ml);
+        }
+        for(int i = mid+1, sum =0;i<=hi;i++){
+            sum+=nums[i];
+            mr = Math.max(sum, mr);
+        }
+        return Math.max(Math.max(lmax, rmax), ml+mr+nums[mid]);
+    }
+
 
     public static void main(String[] args){
         {
